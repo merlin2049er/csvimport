b890b30870d5247fa5930f0bb64e98c0d7327209
joeguerra <zorkmerlin>
2018-01-16 21:39:13 -0500

    works now, needed to fix the hash to the table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,12 @@ class User < ApplicationRecord
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
 
-      User.create! row.to_hash
-    end
+      formatted_row = row.each_with_object({}) do  |(key,value), hash|
+        hash[key.downcase] = value
+      end
 
+      User.create!(formatted_row).inspect
+    end
   end
 
 end
